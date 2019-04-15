76f9396aaad3675ccb518c6ec553958719025823
Marius <marius.bartcus@gmail.com>
2019-04-15 14:28:14 +0200

    ModelPWR

---
+++ PWR/R/ModelPWR.R
@@ -0,0 +1,24 @@
+source("R/FData.R")
+
+ModelPWR <- setRefClass(
+  "ModelPWR",
+  contains = "FData",
+  # Define the fields
+  fields = list(
+    K = "numeric",
+    # number of regimes
+    p = "numeric"
+  )
+)
+
+ModelPWR <- function(fData, K, p) {
+  new(
+    "ModelPWR",
+    Y = fData$Y,
+    X = fData$X,
+    m = fData$m,
+    n = fData$n,
+    K = K,
+    p = p
+  )
+}
