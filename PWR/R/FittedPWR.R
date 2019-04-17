7b85fb57daebf95991c7d0231c26f04c1fd8801c
Florian Lecocq <florian.lecocq@outlook.com>
2019-04-17 14:30:19 +0200

    Add plots

--- PWR/R/FittedPWR.R
+++ PWR/R/FittedPWR.R
@@ -7,48 +7,39 @@ FittedPWR <- setRefClass(
   ),
   methods = list(
     plot = function() {
-      K <- ncol(paramPWR$beta)
 
-      par(mfrow = c(2, 1))
-      plot.default(modelPWR$Y, type = "l", ylab = "y", xlab = "")
+      yaxislim <- c(mean(modelPWR$Y) - 2 * sqrt(var(modelPWR$Y)), mean(modelPWR$Y) + 2 * sqrt(var(modelPWR$Y)))
+
+      # Time series, regressors, and segmentation
+      plot.default(modelPWR$Y, type = "l", ylab = "y", xlab = "", ylim = yaxislim)
       title(main = "Time series, PWR regimes, and segmentation")
-      colors = rainbow(modelPWR$K)
+      colors <- rainbow(modelPWR$K)
       for (k in 1:modelPWR$K) {
-        model_k = statPWR$regressors[,k]
+        model_k <- statPWR$regressors[, k]
 
-        active_model_k = model_k[statPWR$klas==k]
-        active_period_model_k = modelPWR$X[statPWR$klas==k]
+        index <- statPWR$klas == k
+        active_model_k <- model_k[index]
+        active_period_model_k <- seq(1:modelPWR$m)[index]
 
-        inactive_model_k = model_k[statPWR$klas != k]
-        inactive_period_model_k = modelPWR$X[statPWR$klas != k]
-
-        if (!is.null(active_model_k)){
-          par(new=TRUE)
-          lines(inactive_period_model_k, inactive_model_k,col = couleur[k], type = "p", lty = 3, lwd = .5,pch=20, cex=0.1)
-          par(new=TRUE)
-          lines(active_period_model_k, active_model_k,col = couleur[k], lty = "solid", lwd = 4)
+        if (length(active_model_k) != 0) {
+          lines(model_k, col = colors[k], lty = "dotted", lwd = 1)
+          lines(active_period_model_k, active_model_k, col = colors[k], type = "l", lwd = 3)
         }
       }
 
-
-
-      par(mfrow = c(2, 1))
-      plot.default(modelPWR$Y, type = "l", ylab = "y", xlab = "")
+      # Time series, estimated regression function, and optimal segmentation
+      plot.default(modelPWR$Y, type = "l", ylab = "y", xlab = "", ylim = yaxislim)
       title(main = "Time series, PWR function, and segmentation")
-      gammak=c(0, paramPWR$gamma, n)
-      for (k in 1:K){
-        Ik = (gammak[k]+1):gammak[k+1]
+
+      for (k in 1:modelPWR$K) {
+        Ik = paramPWR$gamma[k] + 1:(paramPWR$gamma[k + 1] - paramPWR$gamma[k])
         segmentk = statPWR$mean_function[Ik]
-        par(new=TRUE)
-        lines(t[Ik], segmentk, col = 'red', type = "l", lty = "solid", lwd = 2)
-      }
-      par(new=TRUE)
-      for (i in 1:length(t[paramPWR$gamma])){
-        par(new=TRUE)
-        abline(v=t[paramRHLP$gamma][i], lty = "dotted", col = 'black', lwd = 2)
+        lines(t(Ik), segmentk, col = colors[k], type = "l", lwd = 3)
       }
 
-
+      for (i in 1:length(paramPWR$gamma)) {
+        abline(v = paramPWR$gamma[i], lty = "dotted", lwd = 2, col = "red")
+      }
     }
   )
 )
