fdb73630898821f3ff141b5391e8cbe86e9e47ed
Florian Lecocq <florian.lecocq@outlook.com>
2019-07-11 18:11:17 +0200

    Update documentation

--- R/fitPWRFisher.R
+++ R/fitPWRFisher.R
@@ -1,13 +1,16 @@
-#' fitPWRFisher implements an optimized dynamic programming algorithm to fit a PWR model.
+#' fitPWRFisher implements an optimized dynamic programming algorithm to fit a
+#' PWR model.
 #'
-#' fitPWRFisher is used to fit a Piecewise Regression (PWR) model by maximum-likelihood via an optimized dynamic programming algorithm. The
-#' estimation performed by the dynamic programming algorithm provides an optimal segmentation of the time series.
+#' fitPWRFisher is used to fit a Piecewise Regression (PWR) model by
+#' maximum-likelihood via an optimized dynamic programming algorithm. The
+#' estimation performed by the dynamic programming algorithm provides an optimal
+#' segmentation of the time series.
 #'
-#' @details fitPWRFisher function implements an optimized dynamic programming algorithm of the PWR model.
-#'   This function starts with the calculation of the "cost matrix" then it
-#'   estimates the transition points given `K` the number of regimes thanks to
-#'   the method `computeDynamicProgram` (method of the class
-#'   [ParamPWR][ParamPWR]).
+#' @details fitPWRFisher function implements an optimized dynamic programming
+#'   algorithm of the PWR model. This function starts with the calculation of
+#'   the "cost matrix" then it estimates the transition points given `K` the
+#'   number of regimes thanks to the method `computeDynamicProgram` (method of
+#'   the class [ParamPWR][ParamPWR]).
 #'
 #' @param X Numeric vector of length \emph{m} representing the covariates/inputs
 #'   \eqn{x_{1},\dots,x_{m}}.
@@ -20,22 +23,22 @@
 #' @seealso [ModelPWR], [ParamPWR], [StatPWR]
 #' @export
 fitPWRFisher = function(X, Y, K, p = 3) {
-  
+
   Lmin <- p + 1
-  
+
   paramPWR <- ParamPWR(X = X, Y = Y, K = K, p = p)
-  
+
   C1 <- costMatrix(Y, paramPWR$phi)
-  
+
   Ck <- paramPWR$computeDynamicProgram(C1, K)
   paramPWR$computeParam()
-  
+
   statPWR <- StatPWR(paramPWR = paramPWR)
-  
+
   # Compute statistics
   statPWR$computeStats(paramPWR)
-  
+
   statPWR$objective = Ck[length(Ck)]
-  
+
   return(ModelPWR(param = paramPWR, stat = statPWR))
 }
